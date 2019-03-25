f96b31dd197751a728cfa80b201fd01a6e0d8794
Guilherme Silvarolli Aliperti <gui.aliperti9@gmail.com>
2019-03-25 17:47:37 -0300

    configs dos botoes com interrupção feita

--- HC05-Controle-Exemplo/Atmel/UART_Test/UART_Test/src/main.c
+++ HC05-Controle-Exemplo/Atmel/UART_Test/UART_Test/src/main.c
@@ -41,13 +41,69 @@
 #define UART_COMM USART0
 #endif
 
+//Configs button A
+#define BUTA_PIO           PIOD
+#define BUTA_PIO_ID        ID_PIOD
+#define BUTA_PIO_IDX       11u
+#define BUTA_PIO_IDX_MASK  (1u << BUTA_PIO_IDX)
+
+//Configs button B
+#define BUTB_PIO           PIOA
+#define BUTB_PIO_ID        ID_PIOA
+#define BUTB_PIO_IDX       6u
+#define BUTB_PIO_IDX_MASK  (1u << BUTB_PIO_IDX)
+
+//Configs button Select
+#define BUTSELECT_PIO           PIOA
+#define BUTSELECT_PIO_ID        ID_PIOA
+#define BUTSELECT_PIO_IDX       24u
+#define BUTSELECT_PIO_IDX_MASK  (1u << BUTSELECT_PIO_IDX)
+
+//Configs button Start
+#define BUTSTART_PIO           PIOA
+#define BUTSTART_PIO_ID        ID_PIOA
+#define BUTSTART_PIO_IDX       2u
+#define BUTSTART_PIO_IDX_MASK  (1u << BUTSTART_PIO_IDX)
+
+
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+volatile bool butA_flag = false;
+volatile bool butB_flag = false;
+volatile bool butSelect_flag = false;
+volatile bool butStart_flag = false;
+
 volatile long g_systimer = 0;
+
+
+void butA_callback(void)
+{
+  butA_flag = true;
+}
+
+void butB_callback(void)
+{
+  butB_flag = true;
+}
+
+void butSelect_callback(void)
+{
+  butSelect_flag = true;
+}
+
+void butStart_callback(void)
+{
+  butStart_flag = true;
+}
+
 
 void SysTick_Handler() {
 	g_systimer++;
 }
 
-//configurando a comunica��o
+//configurando a comunica��o
 void config_console(void) {
 	usart_serial_options_t config;
 	config.baudrate = 9600;
@@ -76,7 +132,7 @@ int usart_get_string(Usart *usart, char buffer[], int bufferlen, int timeout_ms)
 			buffer[counter++] = rx;
 		}
 	}
-	//para saber quando o buffer acabou, o c�digo coloca 0x00 como �ltimo valor do buffer
+	//para saber quando o buffer acabou, o c�digo coloca 0x00 como �ltimo valor do buffer
 	buffer[counter] = 0x00;
 	return counter;
 }
@@ -120,6 +176,117 @@ int hc05_server_init(void) {
 	usart_log("hc05_server_init", buffer_rx);
 }
 
+// Fun��o de inicializa��o do uC
+void init(void)
+{
+
+	//desativa watchdog timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+// Inicializa clock do periférico PIO responsavel pelo botao A
+	pmc_enable_periph_clk(BUTA_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+	pio_configure(BUTA_PIO, PIO_INPUT, BUTA_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  pio_handler_set(BUTA_PIO,
+                  BUTA_PIO_ID,
+                  BUTA_IDX_MASK,
+                  PIO_IT_FALL_EDGE,
+                  butA_callback);
+
+  // Ativa interrupção
+  pio_enable_interrupt(BUTA_PIO, BUTA_IDX_MASK);
+
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUTA_PIO_ID);
+  NVIC_SetPriority(BUTA_PIO_ID, 4); // Prioridade 4
+
+  //**************************************************************
+
+  // Inicializa clock do periférico PIO responsavel pelo botao B
+	pmc_enable_periph_clk(BUTB_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+	pio_configure(BUTB_PIO, PIO_INPUT, BUTB_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  pio_handler_set(BUTB_PIO,
+                  BUTB_PIO_ID,
+                  BUTB_IDX_MASK,
+                  PIO_IT_FALL_EDGE,
+                  butB_callback);
+
+  // Ativa interrupção
+  pio_enable_interrupt(BUTB_PIO, BUTB_IDX_MASK);
+
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUTB_PIO_ID);
+  NVIC_SetPriority(BUTB_PIO_ID, 4); // Prioridade 4
+
+  //**************************************************************
+
+  // Inicializa clock do periférico PIO responsavel pelo botao Select
+	pmc_enable_periph_clk(BUTSELECT_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+	pio_configure(BUTSELECT_PIO, PIO_INPUT, BUTSELECT_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  pio_handler_set(BUTSELECT_PIO,
+                  BUTSELECT_PIO_ID,
+                  BUTSELECT_IDX_MASK,
+                  PIO_IT_FALL_EDGE,
+                  butSelect_callback);
+
+  // Ativa interrupção
+  pio_enable_interrupt(BUTSELECT_PIO, BUTSELECT_IDX_MASK);
+
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUTSELECT_PIO_ID);
+  NVIC_SetPriority(BUTSELECT_PIO_ID, 4); // Prioridade 4
+
+  //**************************************************************
+
+  // Inicializa clock do periférico PIO responsavel pelo botao Start
+	pmc_enable_periph_clk(BUTSTART_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+	pio_configure(BUTSTART_PIO, PIO_INPUT, BUTSTART_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  pio_handler_set(BUTSTART_PIO,
+                  BUTSTART_PIO_ID,
+                  BUTSTART_IDX_MASK,
+                  PIO_IT_FALL_EDGE,
+                  butStart_callback);
+
+  // Ativa interrupção
+  pio_enable_interrupt(BUTSTART_PIO, BUTSTART_IDX_MASK);
+
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUTSTART_PIO_ID);
+  NVIC_SetPriority(BUTSTART_PIO_ID, 4); // Prioridade 4
+	
+}
+
 
 int main (void)
 {
@@ -128,6 +295,7 @@ int main (void)
 	delay_init();
 	SysTick_Config(sysclk_get_cpu_hz() / 1000); // 1 ms
 	config_console();
+	init();
 	
 	#ifndef DEBUG_SERIAL
 	usart_put_string(USART1, "Inicializando...\r\n");
@@ -141,12 +309,13 @@ int main (void)
 	char buffer[1024];
 	
 	while(1) {
-		if(pio_get(PIOA, PIO_INPUT, PIO_PA11) == 0) {
+		if(butA_flag) {
 			button1 = '1';
+			butA_flag = false;
 		} else {
 			button1 = '0';
 		}
-		//esse while existe pois a velocidade do microprocessador � muito mais rapida do que a do bt. Ele existe para fazer o c�digo esperar o buffer do bt estar pronto.
+		//esse while existe pois a velocidade do microprocessador � muito mais rapida do que a do bt. Ele existe para fazer o c�digo esperar o buffer do bt estar pronto.
 		while(!usart_is_tx_ready(UART_COMM));
 		usart_write(UART_COMM, button1);
 		while(!usart_is_tx_ready(UART_COMM));
